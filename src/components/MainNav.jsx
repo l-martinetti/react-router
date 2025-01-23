2e70e701647270e45b703d76bb573950f7733702
l-martinetti <luca.martinetti@outlook.it>
2025-01-23 16:28:58 +0100

    feat: add dynamic generated nav menu

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -1,18 +1,20 @@
 import { NavLink } from 'react-router-dom'
 
+const pagesList = [
+    { id: self.crypto.randomUUID(), path: '/', title: 'Home' },
+    { id: self.crypto.randomUUID(), path: '/chi-siamo', title: 'Chi Siamo' },
+    { id: self.crypto.randomUUID(), path: '/lista-post', title: 'Lista Post' }
+]
+
 const MainNav = () => {
     return (
         <nav className='col-3 mt-5 mx-auto'>
             <ul className='list-group-horizontal list-group-flush d-flex justify-content-between'>
-                <li className='list-group-item'>
-                    <NavLink to='/'>Home</NavLink>
-                </li>
-                <li className='list-group-item'>
-                    <NavLink to='/chi-siamo'>Chi Siamo</NavLink>
-                </li>
-                <li className='list-group-item'>
-                    <NavLink to='/lista-post'>Lista post</NavLink>
-                </li>
+                {pagesList.map(page => (
+                    <li key={page.id} className='list-group-item'>
+                        <NavLink to={page.path}>{page.title}</NavLink>
+                    </li>
+                ))}
             </ul>
         </nav>
     )
