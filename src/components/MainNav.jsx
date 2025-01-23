016fb893a60a74a61e0fc60a0941fc7869dfbcb9
l-martinetti <luca.martinetti@outlook.it>
2025-01-23 16:19:55 +0100

    feat: centralize nav menu

--- src/components/MainNav.jsx
+++ src/components/MainNav.jsx
@@ -2,15 +2,15 @@ import { NavLink } from 'react-router-dom'
 
 const MainNav = () => {
     return (
-        <nav>
-            <ul>
-                <li>
+        <nav className='col-3 mt-5 mx-auto'>
+            <ul className='list-group-horizontal list-group-flush d-flex justify-content-between'>
+                <li className='list-group-item'>
                     <NavLink to='/'>Home</NavLink>
                 </li>
-                <li>
+                <li className='list-group-item'>
                     <NavLink to='/chi-siamo'>Chi Siamo</NavLink>
                 </li>
-                <li>
+                <li className='list-group-item'>
                     <NavLink to='/lista-post'>Lista post</NavLink>
                 </li>
             </ul>
